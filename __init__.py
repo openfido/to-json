a14c764a1045e626f029d3e7d2a91c4807ece46c
David P. Chassin <dchassin@slac.stanford.edu>
2020-11-04 12:34:02 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,10 @@ OUTPUTS
 
   List of JSON output files. If output omitted, output is written to /dev/stdout.
 
+OPTIONS
+
+  -d|--dict   Output data as a dictionary
+  -l|--list   Output data as a list
 """
 import os, csv, pandas, json
 def main(inputs,outputs,options):
@@ -20,13 +24,26 @@ def main(inputs,outputs,options):
 			raise Exception("too many outputs")
 	else:
 		outputs = ["/dev/stdout"]
+	format = "dict"
+	for option in options:
+		if option in ["-d","--dict"]:
+			format = "dict"
+		elif option in ["-l","--list"]:
+			format = "list"
+		else:
+			raise Exception("'{option}' is not a valid JSON format")
 	result = []
 	for file in inputs:
 		if not file:
 			raise Exception("missing input")
 		data = pandas.read_csv(file,header=None)
 		result.append(data)
-	result = pandas.DataFrame(pandas.concat(result)).values.tolist()
+	if format == "list":
+		result = pandas.DataFrame(pandas.concat(result)).values.tolist()
+	elif format == "dict":
+		result = pandas.DataFrame(pandas.concat(result)).to_dict()
+	else:
+		raise Exception(f"format '{format}' is not valid")
 	with open(outputs[0],"w") as fh:
 		json.dump(result,fh,indent=4)
 	return result
